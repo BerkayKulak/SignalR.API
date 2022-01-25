8370031d91069a53820a55fd02af98452fd75783
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-01-25 15:16:44 +0300

    SignalR Server Strongly typed Hubs

---
+++ SignalR.API/Hubs/ProductHub.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using SignalR.API.Models;
+
+namespace SignalR.API.Hubs
+{
+    public class ProductHub:Hub<IProductHub>
+    {
+        public async Task SendProduct(Product p)
+        {
+            await Clients.All.ReceiveProduct(p);
+        }
+    }
+}
