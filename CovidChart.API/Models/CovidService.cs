ee95d6be17b970dd606a1f8c7a8b4776b6898d1b
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-01-25 18:27:06 +0300

    Covid19 Chart Uygulaması-3(CovidService oluşturulması)

---
+++ CovidChart.API/Models/CovidService.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using CovidChart.API.Hubs;
+using Microsoft.AspNetCore.SignalR;
+
+namespace CovidChart.API.Models
+{
+    public class CovidService
+    {
+        private readonly AppDbContext _context;
+        private readonly IHubContext<CovidHub> _hubContext;
+
+        public CovidService(AppDbContext context, IHubContext<CovidHub> hubContext)
+        {
+            _context = context;
+            _hubContext = hubContext;
+        }
+
+        public IQueryable<Covid> GetList()
+        {
+            return _context.Covids.AsQueryable();
+        }
+
+        public async Task SaveCovid(Covid covid)
+        {
+            await _context.Covids.AddAsync(covid);
+            await _context.SaveChangesAsync();
+            await _hubContext.Clients.All.SendAsync("ReceiveCovidList", "data");
+        }
+    }
+}
