1c9c9bc848c1bbb00169f8cd2a283cd4a338c191
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-01-25 18:11:40 +0300

    code first - migration - covid class

--- CovidChart.API/Startup.cs
+++ CovidChart.API/Startup.cs
@@ -11,6 +11,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CovidChart.API.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace CovidChart.API
 {
@@ -26,7 +28,10 @@ namespace CovidChart.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration["ConStr"]);
+            });
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
