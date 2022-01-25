971adc918fdd622f5fcb7e26d419ae7a7a78b2d4
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-01-25 15:26:45 +0300

    SignalR Server Hub (Asp.Net Core Mvc)

---
+++ SignalR.Web/Hubs/MyHub.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR.Web.Hubs
+{
+    public class MyHub:Hub
+    {
+        public async Task SendMessage(string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage",message);
+        }
+    }
+}
--- SignalR.Web/Startup.cs
+++ SignalR.Web/Startup.cs
@@ -8,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using SignalR.Web.Hubs;
 
 namespace SignalR.Web
 {
@@ -23,6 +24,7 @@ namespace SignalR.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSignalR();
             services.AddControllersWithViews();
         }
 
@@ -48,6 +50,7 @@ namespace SignalR.Web
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapHub<MyHub>("/MyHub");
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
